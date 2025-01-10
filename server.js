7ee736e8206276d29bea6f2c2c65bdbf783c0abb
NaraeH <naraehyeon@gmail.com>
2025-01-10 16:58:44 +1100

    로그인기능 공부중

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
 // 1. Settings
 // 1-1. Import modules
-const express = require('express');
-const methodOverride = require('method-override');
-const { MongoClient, ObjectId } = require('mongodb');
+const express = require('express');                   // express library
+const methodOverride = require('method-override');    // HTTP methods
+const { MongoClient, ObjectId } = require('mongodb'); // DB
+const session = require('express-session');           // login: session management middleware
+const passport = require('passport');                 // login: authentication middleware
+const LocalStrategy = require('passport-local');      // login: local strategy
 
 // 1-2. Create an Express instance
 const app = express();
@@ -13,8 +16,50 @@ app.use(express.json());                        // req.body
 app.use(express.urlencoded({extended:true}));   // req.body
 app.use(methodOverride('_method'));
 
-// 1-4. Set template engine
-app.set('view engine', 'ejs');                  // EJS
+// 1-4. passport library setting
+// session settings
+app.use(session({
+  secret: '암호화에 쓸 비번',
+  resave : false,
+  saveUninitialized : false,
+  // cookie : { maxAge : 60 * 60 * 1000 } // 1시간
+}))
+app.use(passport.initialize())
+app.use(passport.session()); 
+
+// Passport authentication strategy
+passport.use(new LocalStrategy(async (입력한아이디, 입력한비번, cb) => {
+  let result = await db.collection('user').findOne({ username : 입력한아이디})
+  if (!result) {
+    return cb(null, false, { message: '아이디 DB에 없음' })
+  }
+  if (result.password == 입력한비번) {
+    return cb(null, result); // 로그인 성공
+  } else {
+    return cb(null, false, { message: '비번불일치' });
+  }
+}));
+
+// When login is successful, save the data to the session
+passport.serializeUser((user, done) => {
+  process.nextTick(() => {
+    done(null, { id: user._id, username: user.username }); 
+  })
+})
+
+// After succeefully loggedin, every time a user makes a request to the server
+passport.deserializeUser( async (user, done) => {
+  console.log("deserializeUser!!!");
+  let result = await db.collection('user').findOne({_id : new ObjectId(user.id) })
+  delete result.password; // 비밀번호는 보안상 삭제
+
+  process.nextTick(() => {
+    return done(null, result); // 쿠키 이상 없으면 유저 정보 반환
+  })
+})
+
+// 1-5. Set template engine
+app.set('view engine', 'ejs'); // EJS
 
 // 2. DB connection
 let db
@@ -124,7 +169,10 @@ app.put("/edit/:id", async (req, res) => {
   // 1. Update
   const post = await db.collection('post').updateOne(
     { _id: new ObjectId(req.params.id) },
-    { $set: res.body }
+    { $set: {
+      title: req.body.title,
+      content: req.body.content
+    } }
   );
   
   // 2. Render
@@ -139,3 +187,33 @@ app.delete("/delete", async (req, res)=> {
 
   res.send("삭제완료");
 })
+
+app.get("/login", (req, res) => {
+  res.render("login.ejs");
+})
+
+app.post("/login", async (req, res, next) =>{
+  passport.authenticate('local', (error, user, info) => {
+    if(error) return res.status(500).json(error);
+    if(!user) return res.status(401).json(info.message);
+    
+    //로그인
+    req.login(user, (err) => {
+       
+      if(err) return next(err);
+      res.redirect('/'); 
+    })
+  })(req, res, next);
+});
+
+app.get("/mypage", (req, res)=>{
+  console.log(req.session);
+  console.log(req.isAuthenticated())
+
+  if( req.isAuthenticated() ) {
+    res.render("mypage.ejs", {user: req.user});
+  } else {
+    res.redirect("/login")
+  }
+
+});
