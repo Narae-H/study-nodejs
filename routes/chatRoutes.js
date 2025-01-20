d5e2bca7290c0ac1d85b118562964d80cb664736
NaraeH <naraehyeon@gmail.com>
2025-01-20 17:04:58 +1100

    processing code refactoring

--- routes/chatRoutes.js
+++ routes/chatRoutes.js
@@ -1,28 +1,57 @@
 // 1. Import router
 const router = require('express').Router();
+const { ObjectId } = require('mongodb'); // DB
 
 // 2. Import DB
 const connectDB = require('./../models/mongodb.js');
 let db
 connectDB.then((client)=>{
   console.log('DB연결성공')
-  db = client.db('forum');
+  // db = client.db('forum');
+  db = client.db('forum').collection('chatroom');
 }).catch((err)=>{
   console.log(err)
 }); 
 
-// 3. Router
-// Create a chatroom
-router.get("/:id", (req, res) => {
-  console.log( '채팅방이 생성되었습니다' );
-  console.log( req.params.id );
+// 3. Middleware
+const isLoggedin = (req, res, next) => {
+  if( req.isAuthenticated() ) {
+    next();
+  } else {
+    res.redirect("/login");
+  }
+}
 
-  res.send("채팅방");
-})
+// 4. Router
+// Create a chatroom
+router.get("/request", (req, res) => {
+  // db.collection('chatroom').insertOne({ 
+  db.insertOne({ 
+    member: [req.user._id, new ObjectId(req.query.writerId)],
+    date: new Date(),
+    // postId: new ObjectId(req.params.id)
+  });
+  res.redirect("/chat/myChatList");
+});
 
 // Chat list
-router.get('/myChatList', (res, req) => {
-  res.send('내가 속한 채팅방 목록');
+router.get('/myChatList', isLoggedin, async (req, res) => {
+  let chatlist = await db.find({ 
+  // let chatlist = await db.collection('chatroom').find({ 
+    member: req.user._id 
+  }).toArray();
+
+  res.render("chatlist.ejs", {chatlist: chatlist});
+});
+
+// Details of a chat
+router.get("/detail/:id", isLoggedin, async (req, res)=> {
+  let chatDetail = await db.findOne({
+  // let chatDetail = await db.collection('chatroom').findOne({
+    _id: new ObjectId(req.params.id)
+  });
+
+  res.render("chatDetail.ejs", {chatDetail: chatDetail});
 });
 
 module.exports = router; 
